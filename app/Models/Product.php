591e73809149c8807728917ef823ec383954ee77
Winner13KS <kuandyksanzar020@gmail.com>
2022-10-05 15:50:37 +0600

    Item, Order tables created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Item;
 
 class Product extends Model
 {
@@ -15,6 +16,7 @@ class Product extends Model
      * $this->attributes['price'] - int - contains the product price
      * $this->attributes['created_at'] - timestamp - contains the product creation date
      * $this->attributes['updated_at'] - timestamp - contains the product update date
+     * $this->items - Item[] - contains the associated items
      */
 
 
@@ -27,6 +29,22 @@ class Product extends Model
     //     'price',
     //     'image',
     // ];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+    
+    public function setItems($items)
+    {
+        $this->items = $items;
+    }
+
 
     public static function validate($request)
     {
